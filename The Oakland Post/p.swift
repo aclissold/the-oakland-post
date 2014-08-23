9d26421d5b8553a19bc5916305a10f874b6ba488
Andrew Clissold <andrewclissold@gmail.com>
2014-08-23 15:46:12 -0400

    Add p.swift

---
+++ The Oakland Post/p.swift
@@ -0,0 +1,11 @@
+//
+//  p.swift
+//  The Oakland Post
+//
+//  Created by Andrew Clissold on 8/23/14.
+//  Copyright (c) 2014 Andrew Clissold. All rights reserved.
+//
+
+func p<T>(object: T) {
+    println(object)
+}
