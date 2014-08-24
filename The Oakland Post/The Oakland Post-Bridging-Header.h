4fe18f8ca6dbb90663c2e0ab02d3196a670ccae7
Andrew Clissold <andrewclissold@gmail.com>
2014-08-23 20:36:12 -0400

    Add Parse

--- The Oakland Post/The Oakland Post-Bridging-Header.h
+++ The Oakland Post/The Oakland Post-Bridging-Header.h
@@ -2,6 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import <Parse/Parse.h>
+
 #import "MWFeedParser.h"
 #import "NHBalancedFlowLayout.h"
 #import "NHLinearPartition.h"
