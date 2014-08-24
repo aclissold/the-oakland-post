41284f4811935e6f1d2bbad4c15e8c9d0715932e
Andrew Clissold <andrewclissold@gmail.com>
2014-08-24 17:53:59 -0400

    Login: create SignUpViewController

---
+++ The Oakland Post/SignUpViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpViewController.swift
+//  The Oakland Post
+//
+//  Created by Andrew Clissold on 8/24/14.
+//  Copyright (c) 2014 Andrew Clissold. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(title: "Done", style: .Done, target: self, action: "dismiss:")
+    }
+
+    func dismiss(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
