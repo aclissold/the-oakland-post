83d8d41091ed6f193b48bea58fd92af1e29a73bc
Andrew Clissold <andrewclissold@gmail.com>
2014-09-14 23:27:30 -0400

    Swap placeholder label with placeholder carousel

--- The Oakland Post/BiosViewController.swift
+++ The Oakland Post/BiosViewController.swift
@@ -6,11 +6,41 @@
 //  Copyright (c) 2014 Andrew Clissold. All rights reserved.
 //
 
-class BiosViewController: UIViewController {
+class BiosViewController: UIViewController, iCarouselDataSource, iCarouselDelegate {
 
     @IBOutlet weak var infoToolbar: UIToolbar!
     @IBOutlet weak var toolbarVerticalConstraint: NSLayoutConstraint!
     @IBOutlet weak var toolbarHeightConstraint: NSLayoutConstraint!
+
+    @IBOutlet weak var carousel: iCarousel!
+    override func viewDidLoad() {
+        carousel.type = .Linear
+        carousel.dataSource = self
+        carousel.delegate = self
+    }
+
+    // MARK: iCarouselDataSource
+
+    func numberOfItemsInCarousel(carousel: iCarousel!) -> Int {
+        return 15
+    }
+
+    func carousel(carousel: iCarousel!, viewForItemAtIndex index: Int, reusingView view: UIView!) -> UIView! {
+        let hue = CGFloat(index)/CGFloat(numberOfItemsInCarousel(carousel))
+
+        if view == nil {
+            let newView = UIView(frame: CGRect(x: 0, y: 0, width: 160, height: 160))
+            newView.backgroundColor = UIColor(white: 0.25, alpha: 1)
+            let subview = UIView(frame: CGRect(x: 15, y: 15, width: 130, height: 130))
+            subview.backgroundColor = UIColor(hue: hue, saturation: 1, brightness: 1, alpha: 1)
+            newView.addSubview(subview)
+            return newView
+        }
+
+        (view.subviews.first as UIView).backgroundColor = UIColor(hue: hue, saturation: 1, brightness: 1, alpha: 1)
+
+        return view
+    }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == biosID {
